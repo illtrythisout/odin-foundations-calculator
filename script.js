1cb4aca94688d3e2c180e19c797454699b5c3a9e
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-08-26 16:22:02 +0200

    Create basic functions for operations and operate command
    Create functions for all basic operators (add, sub, multiply, div)
    Added three variables, num1, operator, num2
    Create a function to execute operations based on the chosen operator

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
+
+let num1;
+let operator;
+let num2;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
