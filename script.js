60596e2fb9e4a2166e7324dfca2f7a2b267bb16f
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-08-27 23:50:38 +0200

    Create number history
    Used arrays to create number history for better user experience

--- script.js
+++ script.js
@@ -19,7 +19,40 @@ let num1 = "";
 let operator = "";
 let num2 = "";
 
-const primaryDisplay = document.querySelector("#primaryNum")
+const primaryDisplay = document.querySelector("#primaryNum");
+
+const scndNum1 = document.querySelector("#scndNum1");
+const scndOp1 = document.querySelector("#scndOp1");
+const scndNum2 = document.querySelector("#scndNum2");
+const scndOp2 = document.querySelector("#scndOp2");
+const scndNum3 = document.querySelector("#scndNum3");
+const scndOp3 = document.querySelector("#scndOp3");
+
+let secondaryAnswers = [];
+let secondaryAnsOperator = [];
+let currentNum = "";
+
+function addToSecondaryArray() {
+    secondaryAnswers.unshift(currentNum);
+    currentNum = "";
+    secondaryAnsOperator.unshift(operator);
+    if (secondaryAnswers.length > 3) {
+        secondaryAnswers.pop();
+    }
+    if (secondaryAnsOperator.length > 4) {
+        secondaryAnsOperator.pop();
+    }
+
+    scndNum1.textContent = secondaryAnswers[0];
+    scndOp1.textContent = secondaryAnsOperator[1];
+    scndNum2.textContent = secondaryAnswers[1];
+    scndOp2.textContent = secondaryAnsOperator[2];
+    scndNum3.textContent = secondaryAnswers[2];
+    scndOp3.textContent = secondaryAnsOperator[3];
+
+    console.log(secondaryAnswers)
+    console.log(secondaryAnsOperator)
+}
 
 // Execute operation function based on the chosen operator
 function operate(num1, operator, num2) {
@@ -37,9 +70,14 @@ function operate(num1, operator, num2) {
     }
 }
 
-const buttons = document.querySelectorAll("button")
+// Un-highlight operator buttons
+function unHighlight() {
+    buttons.forEach(btn => {btn.style.backgroundColor = "rgb(240, 240, 240)"})
+}
+
+const buttons = document.querySelectorAll("button");
 buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", () => { // number and decimal point buttons
         if (!isNaN(btn.value) || btn.value === ".") {
             if (operator) {
                 num2 += btn.value;
@@ -48,31 +86,54 @@ buttons.forEach(btn => {
                 num1 += btn.value;
                 primaryDisplay.textContent = num1;
             }
-        } else if (btn.className === "operator") {
+            currentNum += btn.value;
+
+            unHighlight()
+
+        } else if (btn.className === "operator") { // operator buttons
             if (num1 && num2) {
                 num1 = operate(num1, operator, num2);
                 num2 = "";
                 primaryDisplay.textContent = num1;
             }
             operator = btn.value;
-        } else if (num1 && operator && num2) {
+
+            // highlight background
+            buttons.forEach(btn => {btn.style.backgroundColor = "rgb(240, 240, 240)"})
+            btn.style.backgroundColor = "rgb(220, 220, 220)"
+
+            addToSecondaryArray();
+
+        } else if (num1 && operator && num2) { // equal button
             num1 = operate(num1, operator, num2);
             num2 = "";
             primaryDisplay.textContent = num1;
             operator = ""
-        } else if (btn.value === "ac") {
+
+            addToSecondaryArray();
+
+            unHighlight()
+
+        } else if (btn.value === "ac") { // clear button
             num1 = "";
             operator = "";
             num2 = "";
             primaryDisplay.textContent = "0";
-        } else if (btn.value === "del") {
+
+            unHighlight()
+
+        } else if (btn.value === "del") { // backspace button
             if (num2) {
-                num2 = num2.slice(0, -1)
-                primaryDisplay.textContent = num2 || 0
+                num2 = num2.slice(0, -1);
+                primaryDisplay.textContent = num2 || 0;
+            } else if (operator) {
+                operator = ""
             } else if (num1) {
-                num1 = num1.slice(0, -1)
-                primaryDisplay.textContent = num1 || 0
+                num1 = num1.slice(0, -1);
+                primaryDisplay.textContent = num1 || 0;
             }
+
+            unHighlight()
         }
     })
 })
