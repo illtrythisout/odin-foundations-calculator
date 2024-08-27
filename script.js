d21adfe6a16c6188cf558b40854d400814622c00
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-08-27 16:07:17 +0200

    Give functionality to buttons and remove plusMinus
    Was able to wire number buttons to operators
    Removed plusMinus as I couldnt make it work properly

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        return "Error"
+    } else {
+        return a / b
+    }
 }
 
-let num1 = 0;
-let operator = 0;
-let num2 = 0;
+let num1 = "";
+let operator = "";
+let num2 = "";
+
+const primaryDisplay = document.querySelector("#primaryNum")
 
 // Execute operation function based on the chosen operator
 function operate(num1, operator, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
     switch (operator) {
         case "+":
             return add(num1, num2);
@@ -32,19 +40,39 @@ function operate(num1, operator, num2) {
 const buttons = document.querySelectorAll("button")
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
-        num2 = Number(num2 + btn.value);
-        console.log(num2)
+        if (!isNaN(btn.value) || btn.value === ".") {
+            if (operator) {
+                num2 += btn.value;
+                primaryDisplay.textContent = num2;
+            } else {
+                num1 += btn.value;
+                primaryDisplay.textContent = num1;
+            }
+        } else if (btn.className === "operator") {
+            if (num1 && num2) {
+                num1 = operate(num1, operator, num2);
+                num2 = "";
+                primaryDisplay.textContent = num1;
+            }
+            operator = btn.value;
+        } else if (num1 && operator && num2) {
+            num1 = operate(num1, operator, num2);
+            num2 = "";
+            primaryDisplay.textContent = num1;
+            operator = ""
+        } else if (btn.value === "ac") {
+            num1 = "";
+            operator = "";
+            num2 = "";
+            primaryDisplay.textContent = "0";
+        } else if (btn.value === "del") {
+            if (num2) {
+                num2 = num2.slice(0, -1)
+                primaryDisplay.textContent = num2 || 0
+            } else if (num1) {
+                num1 = num1.slice(0, -1)
+                primaryDisplay.textContent = num1 || 0
+            }
+        }
     })
 })
-
-// listen to when user presses button
-// put it in num1
-// display it on main screen
-// if user presses another button, concatinate it to num1
-// when user presses an operator
-// turn num1 into a number
-// store the operator in the operator variable
-// if the user presses another variable, store that instead
-// once user presses a number button again, add num 1 as the first number in the history array
-// put the number the user pressed in num 2
-// when user presses equal or another operator, show the ans and make num two the first in the history array
